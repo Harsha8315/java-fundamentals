c2ae96db33826f4afd2e2dbd13ff555637ecc76a
Harsha8315 <harshavardhans535@gmail.com>
2026-01-20 16:19:40 +0530

    Initial commit

---
+++ src/com/java/fundamentals/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.java.fundamentals;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("My first java code");
+    }
+}
+class WelcomeMessage {
+     public static void main(String[] args){
+         System.out.println("Welcome");
+     }
+}
